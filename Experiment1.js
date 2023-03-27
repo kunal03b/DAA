071440eadb0a6084ac88ca917210c759e4e27c3a
KUNAL SHARMA <kunal03b@gmail.com>
2023-03-28 01:01:05 +0530

    Experiment 1 insertion sort complete

---
+++ Experiment1.js
@@ -0,0 +1,27 @@
+// Implementation of Insertion Sorting technique using Incremental approach.
+function insertionSort(arr,n){
+    let i, key, j;
+    for(i = 0;i < n;i++){
+        key = arr[i];
+        j = i - 1;
+
+        while(j>=0 && arr[j]>key){
+            arr[j+1] = arr[j];
+            j = j -1;
+        }
+        arr[j+1] = key;
+    }
+}
+
+function printArray(arr,n){
+    let i;
+    for(i = 0;i < n;i++){
+        console.log(arr[i] + " ");
+    }
+    console.log("<br>")
+}
+
+let arr  = [12,11,10,6,18,2,3];
+let n = arr.length;
+insertionSort(arr,n);
+printArray(arr,n);
